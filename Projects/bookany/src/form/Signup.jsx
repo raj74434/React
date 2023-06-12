0d3ada9836a3dee97e928b0b191a6f88b74c7305
raj74434 <rajgupta74434@gmail.com>
2023-06-12 23:01:02 +0530

    rounting project added and some changes in form and book any

---
+++ Projects/bookany/src/form/Signup.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+
+const SignupForm = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform form submission or validation logic here
+    console.log('Submitted:', { firstName, lastName, phone, password });
+    // Reset form fields
+    setFirstName('');
+    setLastName('');
+    setPhone('');
+    setPassword('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="firstName">First Name:</label>
+        <input
+          type="text"
+          id="firstName"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="lastName">Last Name:</label>
+        <input
+          type="text"
+          id="lastName"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="phone">Phone:</label>
+        <input
+          type="tel"
+          id="phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+};
+
+export default SignupForm;
--- Projects/form/src/components/Form.jsx
+++ Projects/form/src/components/Form.jsx
@@ -1,35 +1,44 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Form =()=>{
 
-    const[forum,setForum]=useState({
-        first_name:"",
-        last_name:"",
-        age:"",
-        email:""
-    });
+   const [form,setForm] =useState({
+    name:"",
+    email:"",
+    password:"",
+    gender:"",
+    city:""
+   })
 
-    const onClick=(e)=>{
-        const{name,value}=e.target;
-        setForum({...forum ,[name] : value});
-      console.log(e);
-      console.log(forum);
-    }
+   const handleClick=(e)=>{
+    console.log(e);
+    console.log(form);
+    const{name,value}=e.target;
+    setForm({...form,[name]:value});
+   }
+//    useEffect(
+//     console.log(form)
+//    ,[form]);
 
-    return(
-        <div>
-            <form>
-                <label>First Name :</label>
-                <input type="text" placeholder="Enter your first name ..."
-                onChange={onClick}
-                name="first_name"
-                >
+   const formSubmit=(e)=>{
+     e.preventDefault();
+   }
 
-                </input>
-            </form>
 
-        </div>
-    )
+   return (
+    <div>
+        <h1>Sign up</h1> 
+        <form type="submit">
+            <div>
+                <label > Enter your name</label>
+                <input name="name" type="text" onChange={handleClick} ></input>
+            </div>
+            
+
+            <input type="submit" onClick={formSubmit} ></input>
+        </form>
+    </div>
+   )
 
 }
 
